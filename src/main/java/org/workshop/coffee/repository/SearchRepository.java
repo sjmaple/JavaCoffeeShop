96a435601f0832637fc1178ec6446df43ee2fd6d
sjmaple <sjmaple@gmail.com>
2023-09-27 19:18:17 +0100

    Fixed SQLi

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -22,10 +22,16 @@ public class SearchRepository {
         // lowercase the input
         var lowerInput = input.toLowerCase(Locale.ROOT);
         // create a string query that matches the input to the product name or description
-        var query = "SELECT * FROM product WHERE LOWER(product_name) LIKE '%" + lowerInput + "%' OR LOWER(description) LIKE '%" + lowerInput + "%'";
+        //var query = "SELECT * FROM product WHERE LOWER(product_name) LIKE '%" + lowerInput + "%' OR LOWER(description) LIKE '%" + lowerInput + "%'";
+
+        // create a string query with named parameters that matches the input to the product name or description
+        var query = "SELECT * FROM product WHERE LOWER(product_name) LIKE :input OR LOWER(description) LIKE :input";
 
         // create a native query from the string query
         var nativeQuery = em.createNativeQuery(query, Product.class);
+        // set the named parameter to the input
+        nativeQuery.setParameter("input", "%" + lowerInput + "%");
+
         // return the result list
         return nativeQuery.getResultList();
 
