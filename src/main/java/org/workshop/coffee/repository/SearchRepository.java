e3e5aad9439795d6e25017b382e0c16daf430bbb
sjmaple <sjmaple@gmail.com>
2023-10-05 17:23:58 +0200

    Implemented Search

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -23,10 +23,16 @@ public class SearchRepository {
         var lowerInput = input.toLowerCase(Locale.ROOT);
 
         // create string query that matches input to product name or description
-        var query = "SELECT * FROM product WHERE LOWER(product_name) LIKE '%" + lowerInput + "%' OR LOWER(description) LIKE '%" + lowerInput + "%'";
+        //    var query = "SELECT * FROM product WHERE LOWER(product_name) LIKE '%" + lowerInput + "%' OR LOWER(description) LIKE '%" + lowerInput + "%'";
+
+        // create string query using named parameters that matches input to product name or description
+        var query = "SELECT * FROM product WHERE LOWER(product_name) LIKE :input OR LOWER(description) LIKE :input";
 
         // create native query
         var nativeQuery = em.createNativeQuery(query, Product.class);
+
+        // set named parameter
+        nativeQuery.setParameter("input", "%" + lowerInput + "%");
 
         // return list of products
         return nativeQuery.getResultList();
