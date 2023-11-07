31b5e05599ee9ab5903f60728efcd9113da0201a
sjmaple <sjmaple@gmail.com>
2023-11-07 09:26:03 +0000

    Implemented the search and homepage

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,14 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        // lowercase input
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+        // create a query to match my lowerinput to the description or product name
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+        // get results
+        var resultList = (List<Product>) query.getResultList();
+        // return results
+        return resultList;
     }
 
 }
