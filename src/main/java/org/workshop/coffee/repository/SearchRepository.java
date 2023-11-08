aad411da03387e9b474bfb5bec3262225d88d9f9
sjmaple <sjmaple@gmail.com>
2023-11-08 16:25:18 +0000

    Added the Search functionality

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -21,8 +21,13 @@ public class SearchRepository {
     public List<Product> searchProduct (String input) {
         // lowercase the input
         var lowerInput = input.toLowerCase(Locale.ROOT);
-        // create a query that matches the input to the product name or description
-        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+        // create a query with named parameters that matches the input to the product name or description
+        var query = em.createNativeQuery("Select * from Product where lower(description) like :input OR lower(product_name) like :input", Product.class);
+        // var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+
+        // set the named parameter
+        query.setParameter("input", "%" + lowerInput + "%");
+
         // get the result list
         var resultList = (List<Product>) query.getResultList();
         // return the result list
